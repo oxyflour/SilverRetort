30662e3c4d44adbd1974583ae91ac748096da68e
oxyflour <oxyflour@hotmail.com>
2026-07-11 11:25:46 +0800

    update icon

--- packages/chat-ui/src/components/ChatInput.tsx
+++ packages/chat-ui/src/components/ChatInput.tsx
@@ -117,10 +117,7 @@ export function ChatInput() {
               }
             >
               {running ? (
-                <>
-                  <StopIcon />
-                  <span>{"\u505c\u6b62"}</span>
-                </>
+                <StopIcon />
               ) : (
                 <SendIcon />
               )}
