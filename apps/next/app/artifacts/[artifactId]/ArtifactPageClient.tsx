d4b498c7213dcb0bc3ad210b02e81fca03989196
BlueMarmle <31011314+bluemarmle@users.noreply.github.com>
2026-07-17 04:37:16 +0200

    Register artifact renderers for popout viewer

--- apps/next/app/artifacts/[artifactId]/ArtifactPageClient.tsx
+++ apps/next/app/artifacts/[artifactId]/ArtifactPageClient.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { ArtifactViewer } from "silverretort-chat-ui";
+import { registerAppArtifactRenderers } from "../../registerArtifactRenderers";
+
+registerAppArtifactRenderers();
 
 export function ArtifactPageClient({ artifactId }: { artifactId: string }) {
   return <ArtifactViewer artifactId={artifactId} />;
